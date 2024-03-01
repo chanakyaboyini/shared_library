7f6d637095f6224db111d2f611b891c41f17dd08
chanu <chanakyaboyini02@gmail.com>
2024-03-01 21:51:05 +0000

    Static code analysis credentialid added

--- StaticCodeAnalysis.groovy
+++ StaticCodeAnalysis.groovy
@@ -1,4 +1,5 @@
-def call (){
-
-    sh 'mvn clean package sonar:sonar'
+def call (credentialsId){
+    withSonarQubeEnv(credentialsId: credentialsId) {
+        sh 'mvn clean package sonar:sonar'
+}
 }
